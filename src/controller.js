a4833e5764a7ea6c35208b9443f06e302627af36
Fernando Corrales <corrales_fernando@hotmail.com>
2024-08-18 12:43:15 -0300

    fix: se modifico el regex del metodo update libros para capturar mejor la excepcion

--- src/controller.js
+++ src/controller.js
@@ -176,7 +176,7 @@ class LibroController {
 
             // Validar que la fecha sea válida
             const fecha = libro.ano_publicacion.split("T")[0]; // Obtener solo la parte de la fecha
-            const regex = /^\d{4}-\d{2}-\d{2}$/;
+            const regex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
             if(!regex.test(fecha)) {
                 return res.status(400).json({
                     message: "El formato de la fecha es inválido. Debe ser 'aaaa-mm-dd'"
